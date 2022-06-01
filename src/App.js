68657167c029b0734b7a282e007a6894146a6f7b
vitalis <vitalismutwiri2gmail.com>
2022-06-01 21:20:28 +0300

    progress

--- src/App.js
+++ src/App.js
@@ -6,7 +6,22 @@ function App() {
       <img className='imgs' src="/images/bg-pattern-top-desktop.svg" alt="topbackground" />
       <img  className='imgs1' src="/images/bg-pattern-bottom-desktop.svg" alt="backgroundbottom" />
       <div className='container' >
-        
+        <div className='section'>
+        <div className='uppersection'>
+          <div className='uppersectionleft'>
+
+          </div>
+
+          <div className='uppersectionright'>
+            <p><span>⭐⭐⭐⭐⭐</span></p>
+            <p><span>⭐⭐⭐⭐⭐</span></p>
+            <p><span>⭐⭐⭐⭐⭐</span> </p>
+            
+          </div>
+
+          </div>
+
+        </div>
       </div>
     </div>
   );
