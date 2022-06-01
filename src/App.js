3de3c0cf0e5f6675491526d1e42e73b7f2035aa5
vitalis <vitalismutwiri2gmail.com>
2022-06-01 22:48:10 +0300

    progress

--- src/App.js
+++ src/App.js
@@ -9,21 +9,57 @@ function App() {
         <div className='section'>
         <div className='uppersection'>
           <div className='uppersectionleft'>
-
+            <h1> 10,000+ of our users love our products.</h1>
+            <p>We only provide great products combined with excellent customer service.
+            See what our satisfied customers are saying about our services.</p>
           </div>
 
           <div className='uppersectionright'>
-            <p><span>⭐⭐⭐⭐⭐</span></p>
-            <p><span>⭐⭐⭐⭐⭐</span></p>
-            <p><span>⭐⭐⭐⭐⭐</span> </p>
+            <p><span>⭐⭐⭐⭐⭐</span>Rated 5 stars in review</p>
+            <p><span>⭐⭐⭐⭐⭐</span>Rated 5 Stars in Report Guru</p>
+            <p><span>⭐⭐⭐⭐⭐</span> Rated 5 Stars in BestTech</p>
             
           </div>
 
           </div>
 
         </div>
+
+        <div className='section'>
+
+        <div className='lowersection'>
+
+          <div className='lowersectionleft' >
+            <div>  
+              <img/>
+             </div>
+             <div>
+               <span></span>
+               <span></span> 
+                
+              </div>
+            <p></p>
+            
+          </div>
+          <div className='lowersectionmiddle'>
+
+          </div>
+          <div className='lowersectionright'>
+
+          </div>
+         
+
+        </div>
+
+
+
       </div>
-    </div>
+        
+        
+      </div>
+
+     </div> 
+ 
   );
 }
 
